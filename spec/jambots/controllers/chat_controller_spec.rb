c88d995f63b3573082150a9f98642b0a0c7ab2b4
Juan Artero <juan.artero@gmail.com>
2023-05-12 01:27:54 +0200

    Refactor ChatControllerSpec removing doubles

--- spec/jambots/controllers/chat_controller_spec.rb
+++ spec/jambots/controllers/chat_controller_spec.rb
@@ -1,67 +1,66 @@
 require "spec_helper"
 
 RSpec.describe Jambots::Controllers::ChatController do
-  describe "#chat" do
-    let(:options) { {bot: "testbot", path: "/path/to/bots", no_pretty: true} }
-    let(:controller) { described_class.new(options) }
-    let(:conversation) { double("conversation") }
-    let(:message) { double("message") }
-    let(:bot) { instance_double("Jambots::Bot", conversations: conversations, load_conversation: conversation, new_conversation: conversation, message: message) }
-    let(:conversations) { double("conversations", last: conversation) }
+  let(:options) { {bot: "test_bot", path: "./spec/fixtures/bots"} }
+  let(:controller) { described_class.new(options) }
 
-    before do
-      allow(Jambots::Bot).to receive(:new).and_return(bot)
-      allow(bot).to receive(:message).with("hello", conversation).and_return({content: "response"})
-    end
-
-    it "sends the query to the bot to get a response" do
-      controller.chat("hello")
-
-      expect(bot).to have_received(:message).with("hello", conversation)
-    end
-
+  describe "#initialize" do
     context "when last conversation is specified" do
-      let(:options) { {last: true} }
+      let(:options) { {bot: "test_bot", path: "./spec/fixtures/bots", last: true} }
 
       it "loads the last conversation from the bot" do
-        expect(bot.conversations).to receive(:last).and_return(conversation)
-        expect(controller.conversation).to eq(conversation)
+        # last conversation in ./spec/fixtures/bots/test_bot/conversations is 2.yml
+        expect(controller.conversation.key).to eq("2")
       end
     end
 
     context "when conversation is specified" do
-      let(:options) { {conversation: "conversation-id"} }
+      let(:options) { {bot: "test_bot", path: "./spec/fixtures/bots", conversation: "1"} }
 
       it "loads the specified conversation from the bot" do
-        expect(bot).to receive(:load_conversation).with("conversation-id").and_return(conversation)
-        expect(controller.conversation).to eq(conversation)
+        expect(controller.conversation.key).to eq("1")
       end
     end
 
     context "when no conversation is specified" do
-      let(:options) { {no_pretty: true} }
+      let(:options) { {bot: "test_bot", path: "./spec/fixtures/bots"} }
+      let(:conversation) { double("Jambots::Conversation") }
 
       it "creates a new conversation" do
-        expect(bot).to receive(:load_conversation).and_return(nil)
-        expect(bot).to receive(:new_conversation).and_return(conversation)
+        allow_any_instance_of(Jambots::Bot).to receive(:new_conversation).and_return(conversation)
+
         expect(controller.conversation).to eq(conversation)
       end
     end
 
     context "when no_pretty is specified" do
-      let(:options) { {} }
-
       it "uses the minimal renderer" do
         expect(controller.renderer.class).to eq(Jambots::Renderers::CliRenderer)
       end
     end
 
     context "when no_pretty is not specified" do
-      let(:options) { {no_pretty: true} }
+      let(:options) { {bot: "test_bot", path: "./spec/fixtures/bots", no_pretty: true} }
 
       it "does not use the pretty renderer" do
         expect(controller.renderer.class).to eq(Jambots::Renderers::MinimalRenderer)
       end
+    end
+  end
+
+  describe "#chat" do
+    before do
+      allow(controller.bot).to receive(:message).with("hello", controller.conversation).and_return({content: "response"})
+
+      allow(controller.renderer).to receive(:render) do |&block|
+        block.call
+      end
+    end
+
+    it "sends the query to the bot to get a response" do
+      controller.chat("hello")
+      expect(controller.bot).to have_received(:message).with("hello", controller.conversation)
+      expect(controller.renderer).to have_received(:render).with(controller.conversation)
     end
   end
 end
